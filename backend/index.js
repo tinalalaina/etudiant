aa5ad33558543f76439af01043b4f916fc02e13d
tinalalaina14 <tinalalaina14@gmail.com>
2024-01-10 23:41:24 -1200

    initiation

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+//import db from "./config/Database.js"
+import UserRoute from "./routes/UserRoute.js";
+import ProductRoute from "./routes/ProductRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
+dotenv.config();
+
+const app = express();
+
+//(async()=>{
+//    await db.sync();
+//})();
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave:false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}));
+
+app.use(cors({
+    credentials: true,
+    origin:'http://localhost:3000'
+}));
+app.use(express.json());
+app.use(UserRoute);
+app.use(ProductRoute);
+app.use(AuthRoute);
+
+app.listen(process.env.APP_PORT, ()=> {
+    console.log('Server up and running...')
+});
