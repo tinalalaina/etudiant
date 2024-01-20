30ce88fe8be1ef7fa79053c672395f5a815bef57
tinalalaina14 <tinalalaina14@gmail.com>
2024-01-19 17:59:22 -1200

    ajouter un seul produit

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,23 +1,27 @@
 import Product  from "../models/ProductModel.js";
 import Users from "../models/UserModel.js";
-
+import { Op } from "sequelize";
 export const getProducts = async (req, res)  =>{
     try {
         let response;
         if(req.role === "admin") {    
       response = await Product.findAll({
+        attributes:['uuid','name','price'],
             include:[{
-                model: Users
+                model: Users,
+                attributes:['name','email']
             }]
         });
      } 
      else {
         response = await Product.findAll({
+            attributes:['uuid','name','price'],
             where:{
                 userId:req.userId
             },
             include:[{
-                model: Users
+                model: Users,
+                attributes:['name','email']
             }]
         });
         }
@@ -29,8 +33,45 @@ export const getProducts = async (req, res)  =>{
 
 }
 
-export const getProductById = (req, res)  =>{
-
+export const getProductById = async (req, res)  =>{
+    try {
+        const product = await Product.findOne({
+            where:{
+                uuid: req.params.id
+            }
+            });
+        if(!product) return res.status(404).json({msg:"erreur ..."});
+        let response;
+        if(req.role === "admin") {    
+      response = await Product.findOne({
+        attributes:['uuid','name','price'],
+        where:{
+            product: product.id
+        },
+            include:[{
+                model: Users,
+                attributes:['name','email']
+            }]
+        });
+     } 
+     else {
+        response = await Product.findAll({
+            attributes:['uuid','name','price'],
+            where:{
+                [Op.and]:[{id: product.id},{ userId:req.userId}]
+               
+            },
+            include:[{
+                model: Users,
+                attributes:['name','email']
+            }]
+        });
+        }
+       res.status(200).json(response);
+       }
+       catch (error){
+    res.status(500).json ({msg:error.message});
+       }
     
 }
 
